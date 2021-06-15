680b38c8b7d07aab575d07434b4f4808d54fd8d9
Alexander <ag.team@gmail.com>
2021-06-15 22:51:28 +0300

    game almost comlete

--- script.js
+++ script.js
@@ -8,21 +8,51 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
  */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-const score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'Start guessing...';
+});
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = 'Enter number!';
+
+    //When you win!
   } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.message').textContent =
       'Congradulatiobs! Nube is correct';
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+    // number is bigger
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'the numer is higher!';
-  } else {
-    document.querySelector('.message').textContent = 'the number is lower!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'the numer is higher!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'You have lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'the number is lower!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You have lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
 //lets check some changes
